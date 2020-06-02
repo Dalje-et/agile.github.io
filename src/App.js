634cdc6e6f26c2d10ed3c6e59ed678c0a4834871
Daljeet Sandu <daljeet-sandu@hotmail.de>
2020-06-02 21:05:44 +0200

    load questions dynamically and filter them based on keyword

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import TextField from "@material-ui/core/TextField";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import green from "@material-ui/core/colors/green";
-import { animations } from 'react-animation'
+import {animations} from 'react-animation'
 import red from "@material-ui/core/colors/red";
+import questions from "./questions";
 
-
-const useStyles = makeStyles((theme) => ({
+const useStyles = theme => ({
   root: {
     maxWidth: "800px",
     margin: "0 auto",
@@ -20,85 +20,77 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
-  },
-}));
+  }
+});
 
-function App() {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
 
-  const style = {
-    animation: animations.popIn
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayedAnswer: this.filterQuestions("")
+    }
+
+    this.onFilterChange = this.onFilterChange.bind(this);
   }
 
-  return (
+  filterQuestions(filter) {
+    const questionCards = [];
+
+    questions.forEach(question => {
+      if (filter === "" || question.question.includes(filter)) {
+        questionCards.push(
+          (
+            <Grid item xs={6}>
+              <Card style={{animation: animations.popIn}}>
+                <CardContent>
+                  <Typography color="textPrimary" gutterBottom>
+                    {question.question}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    • <span style={{color: green[500]}}>{question.answers[0].text}</span><br/>
+                    • <span style={{color: red[500]}}>{question.answers[1].text}</span><br/>
+                    • <span style={{color: red[500]}}>{question.answers[2].text}</span><br/>
+                    • <span style={{color: red[500]}}>{question.answers[3].text}</span><br/>
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          )
+        );
+      }
+    });
+
+    return questionCards;
+  }
+
+  onFilterChange(event) {
+    this.setState({
+      displayedAnswer: this.filterQuestions(event.currentTarget.value)
+    });
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
       <div className={classes.root}>
         <Grid container spacing={3}>
           <Grid item xs={12}>
-            <TextField style={style} id="filled-basic" label="Keyword" variant="outlined" fullWidth />
+            <TextField
+              id="filled-basic"
+              label="Keyword"
+              variant="outlined"
+              onChange={this.onFilterChange}
+              fullWidth/>
           </Grid>
-          <Grid item xs={6}>
-            <Card>
-              <CardContent>
-                <Typography color="textPrimary" gutterBottom>
-                  Question
-                </Typography>
-                <Typography variant="body2" component="p">
-                  {bull}<span style={{ color: green[500] }}>  well meaning and kindly.</span> <br />
-                  {bull}<span style={{ color: red[500] }}>  well meaning and kindly.</span> <br />
-                  {bull}<span style={{ color: red[500] }}>  well meaning and kindly.</span> <br />
-                  {bull}<span style={{ color: red[500] }}>  well meaning and kindly.</span> <br />
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item xs={6}>
-            <Card>
-              <CardContent>
-                <Typography color="textPrimary" gutterBottom>
-                  Question
-                </Typography>
-                <Typography variant="body2" component="p">
-                  {bull}<span style={{ color: green[500] }}>  well meaning and kindly.</span> <br />
-                  {bull}<span style={{ color: red[500] }}>  well meaning and kindly.</span> <br />
-                  {bull}<span style={{ color: red[500] }}>  well meaning and kindly.</span> <br />
-                  {bull}<span style={{ color: red[500] }}>  well meaning and kindly.</span> <br />
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item xs={6}>
-            <Card>
-              <CardContent>
-                <Typography color="textPrimary" gutterBottom>
-                  Question
-                </Typography>
-                <Typography variant="body2" component="p">
-                  {bull}<span style={{ color: green[500] }}>  well meaning and kindly.</span> <br />
-                  {bull}<span style={{ color: red[500] }}>  well meaning  well meaning  well meaning  well meaning  well meaning  well meaning  well meaning  well meaning and kindly.</span> <br />
-                  {bull}<span style={{ color: red[500] }}>  well meaning and kindly.</span> <br />
-                  {bull}<span style={{ color: red[500] }}>  well meaning and kindly.</span> <br />
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item xs={6}>
-            <Card>
-              <CardContent>
-                <Typography color="textPrimary" gutterBottom>
-                  Question
-                </Typography>
-                <Typography variant="body2" component="p">
-                  {bull}<span style={{ color: green[500] }}>  well meaning and kindly.</span> <br />
-                  {bull}<span style={{ color: red[500] }}>  well meaning and kindly.</span> <br />
-                  {bull}<span style={{ color: red[500] }}>  well meaning and kindly.</span> <br />
-                  {bull}<span style={{ color: red[500] }}>  well meaning and kindly.</span> <br />
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
+
+          {this.state.displayedAnswer}
         </Grid>
       </div>
-  );
+    );
+  }
 }
-export default App;
+
+export default withStyles(useStyles) (App);
---
+++ src/questions.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    "question": "What is my name?",
+    "answers": [
+      {
+        "text": "Daljeet",
+        "correct": true
+      },
+      {
+        "text": "Abodi",
+        "correct": false
+      },
+      {
+        "text": "Test",
+        "correct": false
+      },
+      {
+        "text": "Uhlalala",
+        "correct": false
+      }
+    ]
+  },
+  {
+    "question": "How old am I?",
+    "answers": [
+      {
+        "text": "100",
+        "correct": true
+      },
+      {
+        "text": "93",
+        "correct": false
+      },
+      {
+        "text": "12",
+        "correct": false
+      },
+      {
+        "text": "123",
+        "correct": false
+      }
+    ]
+  }
+]
